ef8be36173cdc3a93e79573d9e87413e6b8e0571
rahaf fallatah <rahaf77553@gmail.com>
2025-02-05 21:09:54 +0300

    lab9Q2

---
+++ lab9Q2.py
@@ -0,0 +1,7 @@
+Q1=int(input("what is the product of 7 * 24"))
+while Q1!=168:
+    print("Your Answer is wrong try again..")
+    Q1=int(input("what is the product of 7 * 24"))
+else :
+    print("You answered this Question correctly")
+    
