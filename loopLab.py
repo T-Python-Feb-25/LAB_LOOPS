e0ef786e825e835d1221eace6be70bf82c194a2b
duha alshehri <alshehriduha7@gmail.com>
2025-02-05 20:21:40 +0300

    solved the loop lab and bonus

---
+++ loopLab.py
@@ -0,0 +1,16 @@
+#Loop Lab first question
+
+x = range(45,210)
+
+for i in x:
+    if i == 100:
+        continue
+    elif i == 205:
+        break
+    else:
+        print(i)
+
+
+
+
+
---
+++ loopLab2.py
@@ -0,0 +1,9 @@
+
+#Loop Lab second question
+output = int(input("what is the product of 7 * 24:"))
+
+while output != 168:
+    print("you got it wrong, try again")
+    output = int(input("what is the product of 7 * 24:"))
+else:
+    print("You answered this Question correctly!!")
